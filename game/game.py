8726ac4b777aa31e49debc941438c70cf039d8a1
mtodorov95 <mtodorov95@gmail.com>
2020-11-12 15:33:57 +0200

    bird/jump

--- game/game.py
+++ game/game.py
@@ -2,14 +2,18 @@ import pygame
 import sys
 import os
 
-base_dir = os.getcwd()
-game_dir = os.path.join(base_dir, 'game')
-assets_dir = os.path.join(game_dir, 'assets')
-
 pygame.init()
 
 screen = pygame.display.set_mode((576, 1024))
 clock = pygame.time.Clock()
+
+# Variables
+base_dir = os.getcwd()
+game_dir = os.path.join(base_dir, 'game')
+assets_dir = os.path.join(game_dir, 'assets')
+
+gravity = 0.25
+bird_movement = 0
 
 bg_surface = pygame.image.load(os.path.join(
     assets_dir, 'background-day.png')).convert()
@@ -18,8 +22,12 @@ bg_surface = pygame.transform.scale2x(bg_surface)
 floor_surface = pygame.image.load(
     os.path.join(assets_dir, 'base.png')).convert()
 floor_surface = pygame.transform.scale2x(floor_surface)
-
 floor_x_pos = 0
+
+bird_surface = pygame.image.load(
+    os.path.join(assets_dir, 'bluebird-midflap.png')).convert()
+bird_surface = pygame.transform.scale2x(bird_surface)
+bird_rect = bird_surface.get_rect(center=(100, 512))
 
 
 def draw_floor():
@@ -32,8 +40,16 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bird_movement = 0
+                bird_movement -= 12
 
     screen.blit(bg_surface, (0, 0))
+    bird_movement += gravity
+    bird_rect.centery += bird_movement
+
+    screen.blit(bird_surface, bird_rect)
     floor_x_pos -= 1
     draw_floor()
     if floor_x_pos <= -576:
