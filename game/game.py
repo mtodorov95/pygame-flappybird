08093a82941b9eba96c3672d116dd54434ec3a6c
mtodorov95 <mtodorov95@gmail.com>
2020-11-12 15:21:07 +0200

    floor animation

--- game/game.py
+++ game/game.py
@@ -15,6 +15,18 @@ bg_surface = pygame.image.load(os.path.join(
     assets_dir, 'background-day.png')).convert()
 bg_surface = pygame.transform.scale2x(bg_surface)
 
+floor_surface = pygame.image.load(
+    os.path.join(assets_dir, 'base.png')).convert()
+floor_surface = pygame.transform.scale2x(floor_surface)
+
+floor_x_pos = 0
+
+
+def draw_floor():
+    screen.blit(floor_surface, (floor_x_pos, 900))
+    screen.blit(floor_surface, (floor_x_pos + 576, 900))
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -22,5 +34,9 @@ while True:
             sys.exit()
 
     screen.blit(bg_surface, (0, 0))
+    floor_x_pos -= 1
+    draw_floor()
+    if floor_x_pos <= -576:
+        floor_x_pos = 0
     pygame.display.update()
     clock.tick(120)
