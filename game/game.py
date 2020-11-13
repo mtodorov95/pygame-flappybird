bc5732a1a73ccc5956f50a87c1c1baef66a31089
mtodorov95 <mtodorov95@gmail.com>
2020-11-13 10:06:01 +0200

    score

--- game/game.py
+++ game/game.py
@@ -17,6 +17,11 @@ gravity = 0.25
 bird_movement = 0
 game_active = True
 
+score = 0
+high_score = 0
+
+game_font = pygame.font.Font(os.path.join(game_dir, '04B_19.TTF'), 40)
+
 bg_surface = pygame.image.load(os.path.join(
     assets_dir, 'background-day.png')).convert()
 bg_surface = pygame.transform.scale2x(bg_surface)
@@ -96,6 +101,29 @@ def bird_animation():
     return new_bird, new_bird_rect
 
 
+def score_display(game_state):
+    if game_state == 'main_game':
+        score_surface = game_font.render(str(score), True, (255, 255, 255))
+        score_rect = score_surface.get_rect(center=(288, 100))
+        screen.blit(score_surface, score_rect)
+    if game_state == 'game_over':
+        score_surface = game_font.render(
+            f'Score: {score}', True, (255, 255, 255))
+        score_rect = score_surface.get_rect(center=(288, 100))
+        screen.blit(score_surface, score_rect)
+
+        high_score_surface = game_font.render(
+            f'High Score: {high_score}', True, (255, 255, 255))
+        high_score_rect = high_score_surface.get_rect(center=(288, 512))
+        screen.blit(high_score_surface, high_score_rect)
+
+
+def update_score(score, high_score):
+    if score > high_score:
+        high_score = score
+    return high_score
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -105,11 +133,13 @@ while True:
             if event.key == pygame.K_SPACE and game_active:
                 bird_movement = 0
                 bird_movement -= 12
+                score += 1
             if event.key == pygame.K_SPACE and game_active == False:
                 game_active = True
                 pipe_list.clear()
                 bird_rect.center = (100, 512)
                 bird_movement = 0
+                score = 0
         if event.type == SPAWNPIPE:
             pipe_list.extend(create_pipe())
         if event.type == BIRDFLAP:
@@ -131,6 +161,10 @@ while True:
 
         pipe_list = move_pipes(pipe_list)
         draw_pipes(pipe_list)
+        score_display('main_game')
+    else:
+        high_score = update_score(score, high_score)
+        score_display('game_over')
 
     floor_x_pos -= 1
     draw_floor()
